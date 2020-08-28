1505b830eab5b294f06b0af4461b613a3f01031a
Ryan Roman <rtroman14@gmail.com>
2020-08-28 08:20:04 -0600

    added styling & functionality to signin/form component, and added button component

---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import SignIn from "../../components/signin/sign-in.component";
+
+const SignInAndSignOut = () => (
+    <div className="sign-in-and-sign-out">
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignOut;

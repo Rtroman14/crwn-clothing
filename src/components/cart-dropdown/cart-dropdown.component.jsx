97e803184e1123f01250671f042d307f30bfc3ea
Ryan Roman <rtroman14@gmail.com>
2020-09-01 07:31:36 -0600

    added cart-item and connected state.cart between CollectionItem and CartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 import CustomButton from "../custom-button/custom-button.component.jsx";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
+import { connect } from "react-redux";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
-        <div className="cart-items"></div>
+        <div className="cart-items">
+            {cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+// Destructure "cart" from state (root-reducer.js) and "cartItems" from "cart"
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+// Connecting the state to our cart-item from collection-item
+export default connect(mapStateToProps)(CartDropdown);
